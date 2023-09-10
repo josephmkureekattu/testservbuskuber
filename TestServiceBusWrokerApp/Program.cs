ad47568dca3ec212404d2600e25f5bf8bddd2474
Joseph Mathews Kureekkattu <Joseph.Mathews.Kureekkattu@gds.ey.com>
2023-09-10 14:19:14 +0530

    sdfk

--- TestServiceBusWrokerApp/Program.cs
+++ TestServiceBusWrokerApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 
 namespace TestServiceBusWrokerApp
 {
@@ -18,6 +19,26 @@ namespace TestServiceBusWrokerApp
 
             app.MapGet("/api/test", () => "Hello World!");
 
+
+            int numThreads = Environment.ProcessorCount; // Use all available CPU cores
+
+            Thread[] threads = new Thread[numThreads];
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                threads[i] = new Thread(PerformCPUIntensiveTask);
+                threads[i].Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+
+
+
+
             app.Run();
         }
 
@@ -27,5 +48,34 @@ namespace TestServiceBusWrokerApp
                 {
                     services.AddHostedService<Worker>();
                 });
+
+        static void PerformCPUIntensiveTask()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Perform a CPU-intensive task (e.g., calculating prime numbers)
+            long maxPrime = 1000000;
+            for (long num = 2; num <= maxPrime; num++)
+            {
+                bool isPrime = true;
+                for (long divisor = 2; divisor <= Math.Sqrt(num); divisor++)
+                {
+                    if (num % divisor == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    // Uncomment the following line to print prime numbers (may produce a lot of output)
+                    // Console.WriteLine(num);
+                }
+            }
+
+            stopwatch.Stop();
+        }
     }
 }
